03eb341c3b8151e2573bde5abf1966d89248f4e4
katzan <kashkin.alex@gmail.com>
2013-06-03 07:11:12 +0400

    Second show to commit changes

--- egit test/src/de/vogella/git/first/GitTest.java
+++ egit test/src/de/vogella/git/first/GitTest.java
@@ -2,7 +2,7 @@ package de.vogella.git.first;
 
 public class GitTest {
 	 public static void main(String[] args) {
-		    System.out.println("Git is cool");
+		    System.out.println("Git is mega cool");
 	 }
 
 }
