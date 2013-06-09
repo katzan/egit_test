9b31babbdd5763fddc92139d0ba00251a5b663dc
katzan <kashkin.alex@gmail.com>
2013-06-09 11:52:11 +0400

    new commit 09062013

--- egit test/src/de/vogella/git/first/GitTest.java
+++ egit test/src/de/vogella/git/first/GitTest.java
@@ -2,7 +2,7 @@ package de.vogella.git.first;
 
 public class GitTest {
 	 public static void main(String[] args) {
-		    System.out.println("And this test is fifth from Air!!!");
+		    System.out.println("This is yet another test from another day 09062013!");
 	 }
 
 }
